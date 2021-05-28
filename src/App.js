3c05ad1dc10a38edaee60a45ccea14f94b856a02
Habib Vadillo <habibvar@gmail.com>
2021-05-28 18:20:00 +0200

    latest movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [latestMovies, setLatestMovies] = useState(null);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=679c414191058aa97a3f1ce1cd0f7bd9&language=en-US&page=1"
+      )
+      .then((result) => {
+        console.log(result.data.results);
+        setLatestMovies(result.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  let latestMoviesDiv = latestMovies ? (
+    <>
+      <h2>Latest Movies</h2>
+      <div id="latest-movies">
+        {latestMovies.map((movie) => {
+          return (
+            <div className="latest-movie">
+              <img src={`http://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+              <h4>{movie.title}</h4>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  ) : null;
+  return <div className="App">{latestMoviesDiv}</div>;
+};
 
 export default App;
